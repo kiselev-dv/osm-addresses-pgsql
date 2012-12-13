1e88f2725ea0d30c01d6c571f8cc701e94905c9a
kiselev-dv <dmitry.v.kiselev@gmail.com>
2012-12-13 09:15:00 +0100

    Update build_area.sql

--- build_area.sql
+++ build_area.sql
@@ -4,7 +4,7 @@ select src_rel.id as src_id, 'R' as src_type, ST_Multi(ST_BuildArea(ST_LineMerge
 	(select ST_Union(way.linestring) from relations as rel 
 		join relation_members as memb on rel.id = memb.relation_id
 		join ways as way on way.id = memb.member_id
-	where rel.id=src_rel.id and memb.member_type = 'W')
+	where rel.id=src_rel.id and memb.member_type = 'W' and (memb.member_role = 'outer' or memb.member_role = 'inner'))
 ))) as geometry, src_rel.tags as tags
 from relations as src_rel where src_rel.where tags ?| ARRAY['place', 'boundary'];
 
