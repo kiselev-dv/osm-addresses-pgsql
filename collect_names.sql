0c175f1c0f560274bedefa34fec7b21bee0c3ba7
Dmitry Kiselev <dmitry.v.kiselev@gmail.com>
2012-12-13 09:46:13 +0100

    name codes siplify

--- collect_names.sql
+++ collect_names.sql
@@ -3,9 +3,6 @@ RETURNS bool AS
         $$ SELECT $1 ~ '^[[:space:]]*$'; $$
         LANGUAGE sql
         IMMUTABLE;
-COMMENT ON FUNCTION empty(TEXT)
-        IS 'Find empty strings or strings containing only whitespace';
-
 
 create or replace function collect_names(tags IN hstore, name_value OUT Text, tag OUT Text, lang OUT Text )
 RETURNS SETOF record AS $$
@@ -21,7 +18,7 @@ RETURNS SETOF record AS $$
 	
 		FOR tag_row IN select * from each(tags) where "key" like '%name%' LOOP
 			tag := tag_row.key;
-			--lang := find_locale(tag_row.key); --too slow
+			--lang := find_locale(tag_row.key); --do it later, not everyone needs this
 			
 			FOR name_split_row IN select btrim(regexp_split_to_table) from regexp_split_to_table(tag_row.value, E'[\\\\;/\\(\\)]') where not empty(regexp_split_to_table) LOOP
 
@@ -46,10 +43,7 @@ RETURNS Text AS $$
 
 		FOR name_split_row IN 
 			select btrim(lower(str)) from regexp_split_to_table(input_text, E'[:]') as str 
-				join lang_codes lc on ( 	
-					not empty(str) and btrim(lower(str)) = lc.code or (
-						substring( btrim(lower(str)) from 1 for 2) = lc.code 
-						and (lower(str) like '%-latn' or lower(str) like '%-cyrl' or lower(str) like '%-cyr' or lower(str) like '%-lat' )))
+				join lang_codes lc on btrim(lower(str)) = lc.code
 			
 		LOOP
 			result := name_split_row.btrim;		 
--- fill_lang_codes.sql
+++ fill_lang_codes.sql
@@ -7,6 +7,6 @@ delete from lang_codes;
 ('gn'),('gu'),('ht'),('ha'),('he'),('iw'),('hi'),('hu'),('is'),('io'),('id'),('in'),('ia'),('ie'),('iu'),('ik'),('ga'),('it'),('ja'),
 ('jv'),('kn'),('ks'),('kk'),('rw'),('ky'),('rn'),('ko'),('ku'),('lo'),('la'),('lv'),('li'),('ln'),('lt'),('mk'),('mg'),('ms'),('ml'),
 ('mt'),('mi'),('mr'),('mo'),('mn'),('na'),('ne'),('no'),('oc'),('or'),('om'),('ps'),('pl'),('pt'),('pa'),('qu'),('rm'),('ro'),('ru'),
-('sm'),('sg'),('sa'),('sr'),('sh'),('st'),('tn'),('sn'),('ii'),('sd'),('si'),('ss'),('sk'),('sl'),('so'),('es'),('su'),('sw'),('sv'),
+('sm'),('sg'),('sa'),('sr'),('sr-latn'),('sr-cyrl'),('sr-lat'),('sr-cyr'),('sh'),('st'),('tn'),('sn'),('ii'),('sd'),('si'),('ss'),('sk'),('sl'),('so'),('es'),('su'),('sw'),('sv'),
 ('tl'),('tg'),('ta'),('tt'),('te'),('th'),('bo'),('ti'),('to'),('ts'),('tr'),('tk'),('tw'),('ug'),('uk'),('ur'),('uz'),('vi'),('vo'),
 ('wa'),('cy'),('wo'),('xh'),('yi'),('ji'),('yo'),('zu');
